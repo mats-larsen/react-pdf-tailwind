3c4ccc39486c53b676a2bbf00c994b5664ca2ad7
Mats Larsen <44677342+mats-larsen@users.noreply.github.com>
2023-01-24 17:46:56 +0100

    Changed import types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,8 +1,8 @@
-import { TailwindTheme } from 'tailwindcss/tailwind-config';
+import { ThemeConfig } from 'tailwindcss/types/config';
 
 declare type StyleSheet = Record<string, string | number>;
 interface Config {
-    theme: TailwindTheme;
+    theme: ThemeConfig;
 }
 declare function createTw(config?: Config): (input: string) => StyleSheet;
 
